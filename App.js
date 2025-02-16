cea8cfdd9fd653669e96e246898e80e6e6dc6aa2
“Smriti <hari2@ualberta.ca>
2025-02-16 16:55:37 -0700

    commit

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
-import 'react-native-gesture-handler';
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./screens/HomeScreen";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import MedicationListScreen from './MedicationListScreen'; // Import your screens
+import RemindersScreen from './RemindersScreen';
+import ScanScreen from './ScanScreen';
+import React from 'react';
 
 const Stack = createStackNavigator();
 
-export default function App() {
+const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="MedicationList" component={MedicationListScreen} />
+        <Stack.Screen name="Reminders" component={RemindersScreen} />
+        <Stack.Screen name="Scan" component={ScanScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
+
+export default App;
